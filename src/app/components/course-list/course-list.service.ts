7a21c65bb8cb89b25bb37fa7a46eb7e053f19215
Dhruba Chatterjee <dhrubach@hotmail.com>
2017-09-19 07:08:51 +0530

    update(course-list) : refactor rxjs operators in service

--- src/app/components/course-list/course-list.service.ts
+++ src/app/components/course-list/course-list.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
 import { ICourse } from '../../models';
 
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/concatMap';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CourseListService {
@@ -12,17 +15,13 @@ export class CourseListService {
 	constructor(private db: AngularFireDatabase) { } /* tslint:disable-line */
 
 	public fetchListOfCourses(topicId: string): Observable<ICourse[]> {
+		const coursesPerTopic$ = this.db.list(`/coursesPerTopic/${topicId}`);
 
-		const courses$ = this.db.object(`/coursesPerTopic/${topicId}`)
-							.do(console.log);
+		const courses$ = coursesPerTopic$
+			.map<any, FirebaseObjectObservable<ICourse>>(
+				(courses) => courses.map((course) => this.db.object(`/courses/${course.$key}`)))
+			.concatMap((objectObservableArray) => Observable.combineLatest<ICourse>(objectObservableArray));
 
-		courses$.subscribe();
-
-		return this.db.list('/courses', {
-			query: {
-				equalTo: topicId,
-				orderByChild: 'topicId',
-			},
-		});
+		return courses$;
 	}
 }
